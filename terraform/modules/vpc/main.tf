975266774a722f7f8b077d64a2d029e7cb0d598e
Nithinkumar9949 <kamuninithinkumar5991@gmail.com>
2026-01-26 14:17:26 +0530

    Initial commit without terraform cache

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block ="10.0.0.0/24"
+
+    tags = {
+        name = "nithin_vpc"
+    }
+}
+resource "aws_subnet" "my_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block="10.0.0.0/28"
+
+    tags = {
+    name = "nithin_subnet"
+    }
+}
+resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my_vpc.id
+
+}
+resource "aws_route_table" "my_rt" {
+    vpc_id = aws_vpc.my_vpc.id
+    route{
+        cidr_block="0.0.0.0/0"
+        gateway_id=aws_internet_gateway.my_igw.id
+    }
+}
+resource "aws_route_table_association" "my_rta" {
+    subnet_id=aws_subnet.my_subnet.id
+    route_table_id = aws_route_table.my_rt.id
+}
